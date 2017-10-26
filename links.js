35005234eba884640caedf04f74f3e94158f27a4
Nathan Lowe <techwiz96@gmail.com>
2017-10-26 13:41:00 -0400

    Add Sockets link

--- links.js
+++ links.js
@@ -1,4 +1,5 @@
 var links = {
     'github': 'https://github.com/nlowe',
-    'twitter': 'https://twitter.com/_nlowe'
+    'twitter': 'https://twitter.com/_nlowe',
+    'sockets': 'https://beej.us/guide/bgnet/output/html/multipage/index.html'
 }
