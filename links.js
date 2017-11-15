6c206e18bc390da536a4a11a66bb5fa04d06e509
Nathan Lowe <techwiz96@gmail.com>
2017-11-15 15:20:01 -0500

    add link to senior-design presentation p1

--- links.js
+++ links.js
@@ -1,5 +1,6 @@
 var links = {
     'github': 'https://github.com/nlowe',
     'twitter': 'https://twitter.com/_nlowe',
-    'sockets': 'https://beej.us/guide/bgnet/output/html/multipage/index.html'
+    'sockets': 'https://beej.us/guide/bgnet/output/html/multipage/index.html',
+    'sdp1': 'https://docs.google.com/presentation/d/1XGh1ul--W7jA4GagO9ZEY7AhQTzMLt2YYyV60eAC2kg/edit?usp=sharing'
 }
