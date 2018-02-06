4ddbed15b90757dabd1589e7d9ec9414ed90b5c0
Nathan Lowe <techwiz96@gmail.com>
2018-02-06 16:30:13 -0500

    sdp2

--- links.js
+++ links.js
@@ -2,5 +2,6 @@ var links = {
     'github': 'https://github.com/nlowe',
     'twitter': 'https://twitter.com/_nlowe',
     'sockets': 'https://beej.us/guide/bgnet/output/html/multipage/index.html',
-    'sdp1': 'https://docs.google.com/presentation/d/1XGh1ul--W7jA4GagO9ZEY7AhQTzMLt2YYyV60eAC2kg/edit?usp=sharing'
+    'sdp1': 'https://docs.google.com/presentation/d/1XGh1ul--W7jA4GagO9ZEY7AhQTzMLt2YYyV60eAC2kg/edit?usp=sharing',
+    'sdp2': 'https://docs.google.com/presentation/d/1KGqccu-YJAJB7kzotYjSn1LhR83-qyQJ7uAPEuSazsU/edit?usp=sharing'
 }
